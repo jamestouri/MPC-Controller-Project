85a0acfd8a395d2d7896c1db98501180744e6b66
jamestouri <j_touri@yahoo.com>
2018-01-22 20:25:49 -0800

    Finished adding MPC Function

--- src/main.cpp
+++ src/main.cpp
@@ -98,8 +98,8 @@ int main() {
           * Both are in between [-1, 1].
           *
           */
-          double steer_value;
-          double throttle_value;
+            double steer_value = -vars[6]/0.436332;
+            double throttle_value = vars[7];
 
           json msgJson;
           // NOTE: Remember to divide by deg2rad(25) before you send the steering value back.
