524eb6b1dabb7f11cb37924037d904522b49b71e
jamestouri <j_touri@yahoo.com>
2018-02-08 19:37:36 -0800

    Changes to the main

--- src/main.cpp
+++ src/main.cpp
@@ -85,28 +85,59 @@ int main() {
         string event = j[0].get<string>();
         if (event == "telemetry") {
           // j[1] is the data JSON object
-          vector<double> ptsx = j[1]["ptsx"];
-          vector<double> ptsy = j[1]["ptsy"];
+          vector<double> ptsx_car = j[1]["ptsx"];
+          vector<double> ptsy_car = j[1]["ptsy"];
           double px = j[1]["x"];
           double py = j[1]["y"];
           double psi = j[1]["psi"];
           double v = j[1]["speed"];
+          double steer_angle = j[1]["steering_angle"];
+          double throttle =  j[1]["throttle"];
             
-            int num_points = ptsx.size();
-            Eigen::VectorXd eigen_next_x(num_points);
-            Eigen::VectorXd eigen_next_y(num_points);
-
-            for (auto i=0; i < num_points; ++i){
-                eigen_next_x(i) = cos(-psi) * (ptsx[i] - px) - sin(-psi) * (ptsy[i] - py);
-                eigen_next_y(i) = sin(-psi) * (ptsx[i] - px) + cos(-psi) * (ptsy[i] - py);
+            for(int i=0; i < ptsx.size();i++)
+            {
+                double x = ptsx[i] - px;
+                double y = ptsy[i] -py;
+                
+                ptsx_car[i] = x * cos(-psi) - y * sin(-psi);
+                ptsy_car[i] = x * sin(-psi) + y * cos(-psi);
+                
             }
-            
-            auto coeffs = polyfit(eigen_next_x, eigen_next_y, 3);
+
+            Eigen::VectorXd ptsxv = Eigen::VectorXd::Map(ptsx_car.data(), ptsx.size());
+            Eigen::VectorXd ptsyv = Eigen::VectorXd::Map(ptsy_car.data(), ptsy.size());
+
+            auto coeffs = polyfit(ptsxv, ptsyv, 3);
             double cte = polyeval(coeffs, 0);
             double epsi = -atan(coeffs[1]);
             
             Eigen::VectorXd state(6);
-            state << 0, 0, 0, v, cte, epsi;
+            double x0 = 0;
+            double y0 = 0;
+            double dt = 0.1;
+            double v0 = v*0.44704;
+            double a0 = throttle;
+            
+            double psi0 = 0;
+            double delta1 = steer_angle;
+            double epsi0 =epsi;
+            
+            
+            double v1 =  (v0 + a0 * dt);
+            double psi1 = (psi0 + v0 * delta1 / Lf * dt);
+            
+            double x1 = (x0 + v0 * cos(psi0) * dt);
+            double y1 = (y0 + v0 * sin(psi0) * dt);
+            
+            double f1 = coeffs[0] + coeffs[1] * x1 + coeffs[2]*x1 * x1 + coeffs[3] * x1 * x1 * x1;
+            
+            double psides1 = atan(coeffs[1] + (2*coeffs[2] * x1) + (3*coeffs[3] * x1 *x1));
+            
+            double cte1 = ((f1 - x1) + (v1 * sin(psi1) * dt));
+            double epsi1 = ((psi1 - psides1) + v1 * delta1 / Lf * dt);
+            
+            
+            state << x1, y1, psi1, v1, cte1, epsi1;
             
             vector<double> vars = mpc.Solve(state,coeffs);
           
@@ -116,12 +147,18 @@ int main() {
           json msgJson;
           // NOTE: Remember to divide by deg2rad(25) before you send the steering value back.
           // Otherwise the values will be in between [-deg2rad(25), deg2rad(25] instead of [-1, 1].
-          msgJson["steering_angle"] = steer_value;
+          msgJson["steering_angle"] = -steer_value;
           msgJson["throttle"] = throttle_value;
 
           //Display the MPC predicted trajectory 
           vector<double> mpc_x_vals;
           vector<double> mpc_y_vals;
+            
+            for(int i=0; i < N-1; i++ )
+            {
+                mpc_x_vals.push_back(vars[i+2]);
+                mpc_y_vals.push_back(vars[(N-1)+i+2]);
+            }
 
           //.. add (x,y) points to list here, points are in reference to the vehicle's coordinate system
           // the points in the simulator are connected by a Green line
@@ -138,6 +175,13 @@ int main() {
 
           msgJson["next_x"] = next_x_vals;
           msgJson["next_y"] = next_y_vals;
+            
+            double d = 2.5;
+            int num = 25;
+            for (int i=0; i<num; i++) {
+                next_x_vals.push_back(d*i);
+                next_y_vals.push_back(polyeval(coeffs, d*i));
+            };
 
 
           auto msg = "42[\"steer\"," + msgJson.dump() + "]";
